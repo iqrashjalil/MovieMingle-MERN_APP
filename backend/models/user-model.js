75ba1c3ce885e9003bf61e81cf655d2784e00167
Iqrash Jalil <iqrashjalil@gmail.com>
2024-07-08 13:23:31 +0500

    added vercel.json

--- backend/models/user-model.js
+++ backend/models/user-model.js
@@ -42,24 +42,11 @@ userSchema.methods.comparePassword = async function (password) {
   return await bcrypt.compare(password, this.password);
 };
 
-// userSchema.methods.getResetPasswordToken = function () {
-//   const resetToken = crypto.randomBytes(20).toString("hex");
-
-//   this.resetPasswordToken = crypto
-//     .createHash("sha256")
-//     .update(resetToken)
-//     .digest("hex");
-
-//   this.resetPasswordExpire = Date.now() + 15 * 60 * 1000;
-//   return resetToken;
-// };
-
 userSchema.methods.generateToken = function () {
   return jwt.sign({ id: this._id }, process.env.JWT_SECRET_KEY, {
     expiresIn: process.env.JWT_EXPIRE,
   });
 };
-
 const User = mongoose.model("User", userSchema);
 
 export default User;
